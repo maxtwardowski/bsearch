cff07b03be84f9223049cc9d8a534218f242cb90
maxtwardowski <twardowskimax@gmail.com>
2018-06-04 19:54:17 +0200

    Tested for strings

--- bsearch.c
+++ bsearch.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void * bsearch(const void * key, const void * base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
-int cmpint(const void* key, const void* element);
+int cmpint(const void * main, const void * compared);
+int cmpstr(const void * main, const void * compared);
 
 int main(int argc, char const *argv[]) {
+    //char * item;
     int * item;
-    int key = 127;
+    int key = 631;
+    //char key[20] = "lodz";
     int values[] = {1, 56, 89, 125, 630};
-    item = (int *) bsearch (&key, values, 5, sizeof (int), cmpint);
-    if (item == NULL)
-        printf("There is no %d in the array\n", key);
+    //char words[][20] = {"bialystok", "ife", "lodz", "major", "suchodolski"};
+    //item = (char *) bsearch (key, words, 5, 20, (int(*)(const void*, const void*)) strcmp);
+    item = (int *) bsearch (&key, values, 5, sizeof(int), cmpint);
+
+    /*if (item == NULL)
+        printf("Not found - %s\n", key);
     else
-        printf("Got it: %d\n", * item);
+        printf("Got it! - %s\n", item);*/
+
+    if (item == NULL)
+        printf("Not found - %d\n", key);
+    else
+        printf("Got it! - %d\n", * item);
     return 0;
 }
 
 void * bsearch(const void * key, const void * base, size_t nmemb, size_t size, int (* compar)(const void *, const void *)) {
     const void * current_element;
-    int cmpresult,
-        first = 0,
-        last = nmemb,
-        medium;
+    int cmpresult, first = 0, last = nmemb, medium;
     while (first <= last) {
         medium = first + (last - first) / 2;
         current_element = ((const char *) base) + (medium * size);
@@ -31,14 +40,13 @@ void * bsearch(const void * key, const void * base, size_t nmemb, size_t size, i
         else if (cmpresult > 0)
             first = medium + 1;
         else
-            return (int *) current_element;
+            return (void *) current_element;
     }
     return NULL;
 }
 
 int cmpint(const void * main, const void * compared) {
-    int * mainref = (int *) main,
-        * cmpd = (int *) compared;
+    int * mainref = (int *) main, * cmpd = (int *) compared;
     if (* mainref > * cmpd) return 1;
     if (* mainref == * cmpd) return 0;
     return -1;
